65f276b2ca8d8ca3995acefc0a5d08925c776c46
alejo <alejo@LAPTOP-5SQL04DU>
2018-06-22 22:19:39 -0300

    Agregar archivos de proyecto.

---
+++ RUSIA2018/Controller/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RUSIA2018.Models;
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RUSIA2018
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Vivo()
+        {
+            return View();
+        }
+        public IActionResult Resultados()
+        {
+            return View();
+        }
+        public IActionResult Registrarse()
+        {            
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Registrarse(string nickname,string correo)
+        {
+            Registro r = new Registro();
+            r.Nick = nickname;
+            r.Correo = correo;
+            r.MostrarNombre(nickname,correo);
+            return View(r);
+        }
+        public IActionResult Noticias()
+        {
+            return View();
+        }
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ RUSIA2018/Models/Registro.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RUSIA2018;
+
+namespace RUSIA2018.Models
+{
+    public class Registro
+    {
+        private string Nombre;
+
+        public string Nick
+        {
+            get { return Nombre; }
+            set { Nombre = value; }
+        }
+        private string  Mail;
+
+        public string  Correo
+        {
+            get { return Mail; }
+            set { Mail = value; }
+        }
+       
+        public string Contraseña { get; set; }
+
+        public void MostrarNombre(string v, string v2)
+        {
+            //Nick = "Bienvenido : " + v;
+            //Mail = "Suscripción correcta! hemos enviado un correo electronico a :" + v2 +"  para mantenerte informado con los resultados del Mundial.. Ya puedes participar por la entrada a la final del mundial!!";
+            v=Nick;
+            v2=Correo;
+        }
+    }
+}
